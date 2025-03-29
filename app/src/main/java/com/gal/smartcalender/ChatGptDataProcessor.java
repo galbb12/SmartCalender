995db2f6ef88e6951d42c1618afe30f365412499
galbb12 <galbb12@gmail.com>
2025-03-30 01:50:10 +0300

    Saved progress

--- app/src/main/java/com/gal/smartcalender/ChatGptDataProcessor.java
+++ app/src/main/java/com/gal/smartcalender/ChatGptDataProcessor.java
@@ -4,6 +4,12 @@ import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.util.Pair;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 // An helpful class for flowing the data through chatgpt
@@ -17,14 +23,22 @@ public class ChatGptDataProcessor extends DataProcessor {
     //Decode the notification for chatgpt
     @Override
     public void processNotification(StatusBarNotification sbn) {
+        Gson gson = new Gson();
+        Map<String, Object> noti_obj = new HashMap<>();
         String packageName = sbn.getPackageName();
         Log.d("package name", packageName);
+        noti_obj.put("package name", packageName);
         for (String key : sbn.getNotification().extras.keySet()) {
             Object value = sbn.getNotification().extras.get(key);
             if(value != null){
                 Log.d(key, value.toString());
+                noti_obj.put(key, value.toString());
             }
+
         }
+
+        String jsonArray = gson.toJson(noti_obj);
+        Log.d("Json array", jsonArray);
 
 
     }
