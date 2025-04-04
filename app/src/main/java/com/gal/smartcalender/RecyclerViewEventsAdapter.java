c90c67374aebe4263bc07541296a5d2c8919b62d
Gal Bareket <65547029+galbb12@users.noreply.github.com>
2025-04-05 01:06:35 +0300

    Saved progress

--- app/src/main/java/com/gal/smartcalender/RecyclerViewEventsAdapter.java
+++ app/src/main/java/com/gal/smartcalender/RecyclerViewEventsAdapter.java
@@ -3,6 +3,7 @@ package com.gal.smartcalender;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,9 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RecyclerViewEventsAdapter extends RecyclerView.Adapter<RecyclerViewEventsAdapter.ViewHolder> {
-    private final Event[] localDataSet;
+    private final Event[] _localDataSet;
+    private static ArrayList<Event> _checked_events;
 
 
     public static String ZonedDateTimeToHumanReadableStr(ZonedDateTime zonedDateTime) {
@@ -22,12 +26,29 @@ public class RecyclerViewEventsAdapter extends RecyclerView.Adapter<RecyclerView
         return systemZonedDateTime.format(formatter);
     }
 
+    public ArrayList<Event> get_checked_events(){
+        return _checked_events;
+    }
+
+    public void clearSelection() {
+        _checked_events.clear();
+        notifyDataSetChanged();
+    }
+
+    public void selectAll() {
+        _checked_events.clear();
+        _checked_events.addAll(Arrays.asList(_localDataSet));
+        notifyDataSetChanged();
+    }
+
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView eventDescriptionView;
         private final TextView eventStartDate;
         private final TextView eventEndDate;
         private final TextView eventImportance;
         private final TextView eventUrgency;
+        private final CheckBox checkBox;
 
         public ViewHolder(View view) {
             super(view);
@@ -36,6 +57,7 @@ public class RecyclerViewEventsAdapter extends RecyclerView.Adapter<RecyclerView
             eventEndDate = view.findViewById(R.id.endDateTime);
             eventImportance = view.findViewById(R.id.importance);
             eventUrgency = view.findViewById(R.id.urgency);
+            checkBox = view.findViewById(R.id.checkBox);
         }
 
         public void bind(Event event) {
@@ -44,11 +66,25 @@ public class RecyclerViewEventsAdapter extends RecyclerView.Adapter<RecyclerView
             eventEndDate.setText(ZonedDateTimeToHumanReadableStr(event.endDate));
             eventImportance.setText(String.valueOf(event.importance));
             eventUrgency.setText(String.valueOf(event.urgency));
+            checkBox.setOnCheckedChangeListener((compoundButton, b) -> {
+                if(b){
+                    _checked_events.add(event);
+                }else{
+                    if(_checked_events.contains(event)){
+                        _checked_events.remove(event);
+                    }
+                }
+            });
+            if(_checked_events.contains(event)){
+                checkBox.setChecked(true);
+            } else{
+                checkBox.setChecked(false);
+            }
         }
     }
 
     public RecyclerViewEventsAdapter(Event[] dataSet) {
-        localDataSet = dataSet;
+        _localDataSet = dataSet;
     }
 
     @NonNull
@@ -61,11 +97,11 @@ public class RecyclerViewEventsAdapter extends RecyclerView.Adapter<RecyclerView
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-        viewHolder.bind(localDataSet[position]);
+        viewHolder.bind(_localDataSet[position]);
     }
 
     @Override
     public int getItemCount() {
-        return localDataSet.length;
+        return _localDataSet.length;
     }
 }
